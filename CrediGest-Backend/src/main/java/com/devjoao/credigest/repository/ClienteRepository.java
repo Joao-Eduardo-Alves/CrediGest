b0d8b14daa31c56aa99b918d46070697808b939f
Joao-Eduardo-Alves <joaoeduardo.alves@outlook.com>
2026-03-30 22:19:07 -0300

    refactor: removido conteudo nao utilizado em arquivo

--- CrediGest-Backend/src/main/java/com/devjoao/credigest/repository/ClienteRepository.java
+++ CrediGest-Backend/src/main/java/com/devjoao/credigest/repository/ClienteRepository.java
@@ -1,11 +1,8 @@
 package com.devjoao.credigest.repository;
 
 import com.devjoao.credigest.entity.Cliente;
-import com.devjoao.credigest.entity.Fiado;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{}
